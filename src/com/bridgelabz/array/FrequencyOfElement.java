2dd3bb95091812be599fda34e36557f24c63a8e1
sujit <sujitbokade2014@gmail.com>
2022-09-14 20:37:15 +0530

    [Sujit]Program Added

---
+++ src/com/bridgelabz/array/FrequencyOfElement.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.array;
+
+public class FrequencyOfElement {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 4, 2, 2, 6, 1, 4};
+        int[] visitedArr = new int[arr.length];
+
+        int visited = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visitedArr[j] = visited;
+                }
+            }
+            if (visitedArr[i] != visited) {
+                visitedArr[i] = count;
+            }
+        }
+        for (int i = 0; i < visitedArr.length; i++) {
+            if (visitedArr[i] != visited) {
+                System.out.println("Frequency of " + arr[i] + " is: " + visitedArr[i]);
+            }
+        }
+    }
+}
